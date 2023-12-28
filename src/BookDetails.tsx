1f66ffbd5324bc1b649c93ca2dc6e0a15973dc3c
Kasia Maciejewska <katarzyna.maciejewska@swmansion.com>
2023-12-28 13:36:50 +0100

    Add book shelf update option and add basic styles

--- src/BookDetails.tsx
+++ src/BookDetails.tsx
@@ -5,11 +5,24 @@ interface IBook {
   _id: string;
   title: string;
   author: string;
+  exclusiveShelf: string;
 }
 
+interface IShelves {
+  name: string;
+  value: string;
+}
+
+const shelves: IShelves[] = [
+  { name: "Przeczytana", value: "read" },
+  { name: "Obecnie czytana", value: "currently-reading" },
+  { name: "Chcę przeczytać", value: "to-read" },
+];
+
 function BookDetails() {
-  const { id } = useParams();
+  const { id } = useParams<{ id: string }>();
   const [book, setBook] = useState<IBook | null>(null);
+  const [showDropdown, setShowDropdown] = useState(false);
 
   useEffect(() => {
     const fetchBook = async () => {
@@ -25,17 +38,85 @@ function BookDetails() {
     fetchBook();
   }, [id]);
 
+  const handleShelfChange = async (shelf: string) => {
+    if (book) {
+      try {
+        const response = await fetch(
+          `http://localhost:3000/books/${book._id}`,
+          {
+            method: "PATCH",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ shelf: shelf }),
+          },
+        );
+        if (response.ok) {
+          setBook({ ...book, exclusiveShelf: shelf });
+        } else {
+          console.error(`Failed to update shelf for book ${book._id}`);
+        }
+      } catch (err) {
+        console.error(err);
+      }
+    }
+    setShowDropdown(false);
+  };
+
   return (
-    <div className="container p-10">
-      <Link to="/">⬅</Link>
-      {book ? (
-        <div className="mt-6">
-          <h2 className="text-lg font-bold">{book.title}</h2>
-          <p>{book.author}</p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+    <div className="h-screen bg-gray-200">
+      <div className="container mx-auto p-10">
+        <Link to="/" className="text-blue-600 hover:text-blue-800">
+          ⬅
+        </Link>
+        {book ? (
+          <div className="mt-6 rounded bg-white px-5 py-4 shadow">
+            <h2 className="text-lg font-bold text-gray-800">{book.title}</h2>
+            <p className="text-gray-700">{book.author}</p>
+            <div className="mt-3 flex items-center">
+              <span className="relative inline-block">
+                <button
+                  onClick={() => setShowDropdown(!showDropdown)}
+                  className="text-gray-500 hover:text-gray-600"
+                >
+                  🖋️
+                </button>
+                {showDropdown && (
+                  <div className="absolute mt-2 w-48 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5">
+                    {shelves.map((shelf) => (
+                      <a
+                        href="#!"
+                        key={shelf.value}
+                        onClick={(e) => {
+                          e.preventDefault();
+                          handleShelfChange(shelf.value);
+                        }}
+                        className={`block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 ${
+                          book && book.exclusiveShelf === shelf.value
+                            ? "font-bold"
+                            : ""
+                        }`}
+                      >
+                        {shelf.name}
+                      </a>
+                    ))}
+                  </div>
+                )}
+              </span>
+              <span className="ml-2 text-gray-700">
+                {
+                  shelves.find((shelf) => shelf.value === book.exclusiveShelf)
+                    ?.name
+                }
+              </span>
+            </div>
+          </div>
+        ) : (
+          <div className="rounded bg-white px-5 py-4 shadow">
+            <p className="text-gray-500">Loading...</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
--- src/BookList.tsx
+++ src/BookList.tsx
@@ -61,67 +61,77 @@ function BookList() {
   };
 
   return (
-    <div className="container p-10">
-      <button
-        className="mb-2 mr-2 bg-slate-200 px-4 py-2"
-        onClick={() => fetchBooks()}
-      >
-        Wszystkie książki
-      </button>
-      <button
-        className="mb-2 mr-2 bg-slate-200 px-4 py-2"
-        onClick={() => fetchBooks("read")}
-      >
-        Przeczytane
-      </button>
-      <button
-        className="mb-2 mr-2 bg-slate-200 px-4 py-2"
-        onClick={() => fetchBooks("to-read")}
-      >
-        Do przeczytania
-      </button>
-      <button
-        className="mb-2 mr-2 bg-slate-200 px-4 py-2"
-        onClick={() => fetchBooks("currently-reading")}
-      >
-        Obecnie czytane
-      </button>
-      <button
-        className="mb-2 mr-2 bg-slate-200 px-4 py-2"
-        onClick={getRandomBook}
-      >
-        Losuj książkę do przeczytania
-      </button>
-      {randomBook && (
-        <>
-          <div className="mb-4" key={randomBook._id}>
-            <Link to={`/book/${randomBook._id}`}>
-              <h2 className="text-lg font-bold">{randomBook.title}</h2>
+    <div className="h-full min-h-screen bg-gray-200">
+      <div className="container mx-auto p-10">
+        <button
+          className="mb-2 mr-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => fetchBooks()}
+        >
+          Wszystkie książki
+        </button>
+        <button
+          className="mb-2 mr-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => fetchBooks("read")}
+        >
+          Przeczytane
+        </button>
+        <button
+          className="mb-2 mr-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => fetchBooks("to-read")}
+        >
+          Do przeczytania
+        </button>
+        <button
+          className="mb-2 mr-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={getRandomBook}
+        >
+          Losuj książkę do przeczytania
+        </button>
+        {randomBook && (
+          <>
+            <div
+              className="mb-4 rounded bg-white px-5 py-4 shadow"
+              key={randomBook._id}
+            >
+              <Link
+                to={`/book/${randomBook._id}`}
+                className="text-blue-600 hover:text-blue-800"
+              >
+                <h2 className="text-lg font-bold text-gray-800">
+                  {randomBook.title}
+                </h2>
+              </Link>
+              <p className="text-gray-700">{randomBook.author}</p>
+            </div>
+            <button
+              className="mb-2 mr-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+              onClick={startReading}
+            >
+              Zacznij czytać!
+            </button>
+            <button
+              className="mb-2 mr-2 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-700"
+              onClick={getRandomBook}
+            >
+              Losuj następną
+            </button>
+          </>
+        )}
+        {books.map((book) => (
+          <div
+            className="mb-4 rounded bg-white px-5 py-4 shadow"
+            key={book._id}
+          >
+            <Link
+              to={`/book/${book._id}`}
+              className="text-blue-600 hover:text-blue-800"
+            >
+              <h2 className="text-lg font-bold text-gray-800">{book.title}</h2>
             </Link>
-            <p>{randomBook.author}</p>
+            <p className="text-gray-700">{book.author}</p>
           </div>
-          <button
-            className="mb-2 mr-2 bg-slate-200 px-4 py-2"
-            onClick={startReading}
-          >
-            Zacznij czytać!
-          </button>
-          <button
-            className="mb-2 mr-2 bg-slate-200 px-4 py-2"
-            onClick={getRandomBook}
-          >
-            Losuj następną
-          </button>
-        </>
-      )}
-      {books.map((book) => (
-        <div className="mb-4" key={book._id}>
-          <Link to={`/book/${book._id}`}>
-            <h2 className="text-lg font-bold">{book.title}</h2>
-          </Link>
-          <p>{book.author}</p>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 }
