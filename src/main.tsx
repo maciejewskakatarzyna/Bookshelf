9ee7fdf66efa4178e4ac4626e67b20be3225b1e0
Kasia Maciejewska <katarzyna.maciejewska@swmansion.com>
2023-12-28 11:19:29 +0100

    Add prettier & clsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import BookList from "./BookList";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
